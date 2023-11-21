a1e25bc24836baee450a59c155e58c998b921c7b
Hugo Planell <hugoplanellmoreno@gmail.com>
2023-11-21 21:39:22 +0100

    Switch to Advance Expression

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -20,9 +20,11 @@ func _ready():
 	anim_player.play("walk")
 
 func _process(delta):
+	"""
 	anim_tree.set("parameters/conditions/idle", velocity.length() < 0.25)
 	anim_tree.set("parameters/conditions/walk", (velocity.length() > 0.25) and SPEED <= 2.0)
 	anim_tree.set("parameters/conditions/run", SPEED > 2.0)
+	"""
 		
 func _physics_process(delta):
 	if Input.is_action_just_pressed("left_click") and !is_picked:

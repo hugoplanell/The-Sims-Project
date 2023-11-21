df711f188ee2ba929becdbd54ff6835e6c93af1c
Hugo Planell <hugoplanellmoreno@gmail.com>
2023-11-21 21:27:50 +0100

    AnimationTree State Machine Implemented

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,17 @@
 extends CharacterBody3D
 
-const SPEED = 2.0
 const ACCEL = 10
 
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
+@onready var anim_tree : AnimationTree = $AnimationTree
+@onready var playback : AnimationNodeStateMachinePlayback = anim_tree.get("parameters/playback")
 
 var anim_player : AnimationPlayer
 
+var SPEED = 2.0
+
 var is_picked = false
+var is_running = false
 
 func _ready():
 	Global.player_node = self
@@ -16,14 +20,31 @@ func _ready():
 	anim_player.play("walk")
 
 func _process(delta):
-	pass
-
+	anim_tree.set("parameters/conditions/idle", velocity.length() < 0.25)
+	anim_tree.set("parameters/conditions/walk", (velocity.length() > 0.25) and SPEED <= 2.0)
+	anim_tree.set("parameters/conditions/run", SPEED > 2.0)
+		
 func _physics_process(delta):
-	
-	var direction = Vector3()
-	
 	if Input.is_action_just_pressed("left_click") and !is_picked:
 		nav.target_position = Global.player_target_pos
+	
+	player_movement(delta)
+	state_machine()
+
+func state_machine() -> void:
+	match playback.get_current_node():
+		"idle":
+			print("idling")
+		"walk":
+			print("walking")
+			if((nav.target_position - self.global_position).length() > 20):
+				SPEED = 4.0
+		"run":
+			if((nav.target_position - self.global_position).length() < 5):
+				SPEED = 2.0
+
+func player_movement(delta: float):
+	var direction = Vector3()
 	
 	direction = nav.get_next_path_position() - global_position
 	direction = direction.normalized()
@@ -35,10 +56,6 @@ func _physics_process(delta):
 		#esto sigue teniendo la limitacion del anterior porque resto lookdir - rotation.y
 		#if rotation.y != lookdir:
 			#rotate_y((lookdir - rotation.y) / 10)
-		
-		anim_player.current_animation = "walk"
-	else:
-		anim_player.current_animation = "Action_001"
 	
 	velocity = velocity.lerp(direction * SPEED, ACCEL * delta)
 	

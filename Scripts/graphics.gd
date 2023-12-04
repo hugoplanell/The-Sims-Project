4441c6a7f50e225d8db2286243e53ef67a7f8e36
Hugo Planell <hugoplanellmoreno@gmail.com>
2023-12-04 17:33:53 +0100

    Fixes GameOptions

--- Scripts/graphics.gd
+++ Scripts/graphics.gd
@@ -5,26 +5,28 @@ var lights : Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	lights = get_parent().find_children("", "Light3D") # no funciona :(
-	set_low_graphics()
+	lights = get_tree().get_current_scene().find_children("", "Light3D")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	#get_viewport()
+	#RenderingServer.directional_shadow_atlas_set_size(0, true)
 
 func set_low_graphics():
 	ProjectSettings.set_setting("rendering/lights_and_shadows/directional_shadow/size", 4096)
 	ProjectSettings.set_setting("rendering/lights_and_shadows/directional_shadow/soft_shadow_filter_quality", 2)
 	ProjectSettings.set_setting("textures/default_filters/anisotropic_filtering_level", 2)
 	ProjectSettings.set_setting("anti_aliasing/quality/msaa_3d", 0)
-	#shadows(false)
+	shadows(false)
 
 func set_high_graphics():
 	ProjectSettings.set_setting("rendering/lights_and_shadows/directional_shadow/size", 8192)
 	ProjectSettings.set_setting("rendering/lights_and_shadows/directional_shadow/soft_shadow_filter_quality", 5)
 	ProjectSettings.set_setting("textures/default_filters/anisotropic_filtering_level", 3)
 	ProjectSettings.set_setting("anti_aliasing/quality/msaa_3d", 2)
+	shadows(true)
 
 func shadows(enabled: bool):
 	for light: Light3D in lights:

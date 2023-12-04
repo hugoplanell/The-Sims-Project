feba8a88e1081ec790b09d761a9a6a2fc8ef4fb0
Hugo Planell <hugoplanellmoreno@gmail.com>
2023-12-04 20:52:10 +0100

    Prop Building Improved

--- Scripts/States/building_states/prop_state.gd
+++ Scripts/States/building_states/prop_state.gd
@@ -17,34 +17,36 @@ signal nav_mesh_changed
 
 func enter():
 	active = true
-	current_prop = prop_library_resource.scenes[current_prop_idx].instantiate()
-	current_scene.get_node("NavigationRegion3D").add_child.call_deferred(current_prop)
+	current_prop_reference = prop_library_resource.scenes[current_prop_idx].instantiate()
+	#current_scene.get_node("NavigationRegion3D").add_child.call_deferred(current_prop)
+	add_child.call_deferred(current_prop_reference)
 
 func exit():
 	current_prop.queue_free()
 	active = false
 
 func update(_delta: float):
-	if current_prop.is_hovered == false: current_prop.position = mouse_position
+	if current_prop_reference != null and current_prop_reference.is_hovered == false:
+		current_prop_reference.position = mouse_position
 	
 	if Input.is_action_just_pressed("debug_prop_rotate_left"):
-		current_prop.rotate_y(PI/4)
+		current_prop_reference.rotate_y(PI/4)
 	
 	if Input.is_action_just_pressed("debug_prop_rotate_right"):
-		current_prop.rotate_y(-PI/4)
+		current_prop_reference.rotate_y(-PI/4)
 	
 	if Input.is_action_just_pressed("debug_change_prop_up"):
 		if current_prop_idx + 1 < prop_library_resource.scenes.size():
 			current_prop_idx += 1
-			current_prop.queue_free()
-			current_prop = prop_library_resource.scenes[current_prop_idx].instantiate()
-			current_scene.get_node("NavigationRegion3D").add_child(current_prop)
+			current_prop_reference.queue_free()
+			current_prop_reference = prop_library_resource.scenes[current_prop_idx].instantiate()
+			add_child(current_prop_reference)
 	elif Input.is_action_just_pressed("debug_change_prop_down"):
 		if current_prop_idx - 1 >= 0:
 			current_prop_idx -= 1
-			current_prop.queue_free()
-			current_prop = prop_library_resource.scenes[current_prop_idx].instantiate()
-			current_scene.get_node("NavigationRegion3D").add_child(current_prop)
+			current_prop_reference.queue_free()
+			current_prop_reference = prop_library_resource.scenes[current_prop_idx].instantiate()
+			add_child(current_prop_reference)
 		
 
 func physics_update(_delta: float):
@@ -54,9 +56,13 @@ func physics_update(_delta: float):
 func _on_player_camera_mouse_position_3d(position):
 	if active:
 		if Input.is_action_just_pressed("left_click"):
-			current_prop = prop_library_resource.scenes[current_prop_idx].instantiate()
+			current_prop = current_prop_reference.duplicate()
+			#current_prop_reference.queue_free()
 			current_scene.get_node("NavigationRegion3D").add_child(current_prop)
 			nav_mesh_changed.emit()
+			
+			#current_prop_reference = prop_library_resource.scenes[current_prop_idx].instantiate()
+			#add_child(current_prop_reference)
 		
 		#mouse_position = floor(position + Vector3(0.5,1,0.5))
 		mouse_position = Vector3(floor(position.x + 0.5), position.y, floor(position.z + 0.5))

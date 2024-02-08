890bc1f807905263348ed2e1bcf86ae5c8edc501
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-02-08 20:58:08 +0100

    Window Building Added

---
+++ Scripts/States/building_states/window_state.gd
@@ -0,0 +1,41 @@
+class_name WindowBuilding
+extends BuildingState
+
+@onready var current_scene = get_tree().get_current_scene() #mirar mejores formas de hacer esto(valorar un global)
+var prop_scene : PackedScene
+
+var prop : CSGBox3D = null
+var prop_reference : CSGBox3D = null
+
+func _ready():
+	prop_scene = preload("res://Assets/Models/Props/window_1/window_1.tscn")
+	prop_reference = prop_scene.instantiate()
+	#current_scene.get_node("NavigationRegion3D").add_child.call_deferred(current_prop)
+	add_child.call_deferred(prop_reference)
+
+func _exit_tree():
+	prop_reference.queue_free()
+
+func _process(delta):
+	if prop_reference != null and prop_reference.is_hovered == false:
+		prop_reference.global_position = mouse_position
+
+func _physics_process(delta):
+	pass
+
+func _on_player_camera_mouse_3d(position, body):
+	if Input.is_action_just_pressed("left_click"):
+		prop = prop_reference.duplicate()
+		#current_prop_reference.queue_free()
+		prop.is_placed = true
+		prop.global_position = mouse_position - body.global_position #revisar esto
+		body.add_child(prop)
+		print(body)
+		nav_mesh_changed.emit()
+		
+		#current_prop_reference = prop_library_resource.scenes[current_prop_idx].instantiate()
+		#add_child(current_prop_reference)
+	
+	#mouse_position = floor(position + Vector3(0.5,1,0.5))
+	mouse_position = Vector3(floor(position.x + 0.5), position.y, floor(position.z + 0.5))
+
